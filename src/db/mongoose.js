53e5af5deb839e3ce3cf8b54ca58deb52d7b13fe
Sanghita1 <sanghitagharami2000@mal.com>
2023-09-10 17:38:30 +0530

    final update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://localhost:27017/wisemeal-api", {useNewUrlParser:true, useUnifiedTopology:true, autoIndex:true});
+// mongoose.connect("mongodb://localhost:27017/nutrifit-api", {useNewUrlParser:true, useUnifiedTopology:true, autoIndex:true});
 
-mongoose.connect(`mongodb+srv://Nutrifit123:${process.env.MONGOOSE_PASS}@cluster0.urccrhz.mongodb.net/nutrifit-api?retryWrites=true&w=majority`, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
+// mongoose.connect(`mongodb+srv://Nutrifit123:Nutrifit369@cluster0.urccrhz.mongodb.net/nutrifit-api?retryWrites=true&w=majority`, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// })
+
+// mongoose.connect(`mongodb+srv://arindam369:<password>@cluster0.hspwrdh.mongodb.net/?retryWrites=true&w=majority`, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// })
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.hspwrdh.mongodb.net/nutrifit-api?retryWrites=true&w=majority`, {useNewUrlParser:true, useUnifiedTopology:true});
